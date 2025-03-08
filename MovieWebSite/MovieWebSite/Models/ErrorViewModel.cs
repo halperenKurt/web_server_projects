f3f5451d630879a64b777e270f57cb8ba7af328f
halperenKurt <kurtalperen96@gmail.com>
2025-03-08 13:16:00 +0300

    movie project is created.

---
+++ MovieWebSite/MovieWebSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MovieWebSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
